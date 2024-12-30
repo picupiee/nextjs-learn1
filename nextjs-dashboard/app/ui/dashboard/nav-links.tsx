7de3b27f77427e96fb82ae9dc982ba67d16d90f8
picupiee.id <picupiee.id@gmail.com>
2024-12-30 05:34:03 +0000

    updating nav-links for sidebar

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -19,6 +19,7 @@ const links = [
     icon: DocumentDuplicateIcon,
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Database Form', href: '/dashboard/form', icon: DocumentDuplicateIcon },
 ];
 
 export default function NavLinks() {
